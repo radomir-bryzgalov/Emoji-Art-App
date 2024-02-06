1b3057b54adc6b9bedcedcc5657214bd89ea6579
Radomir Bryzgalov <rb8745359@gmail.com>
2024-02-06 09:21:39 -0500

    lec 13 27:11

---
+++ Emoji Art/PalleteEditor.swift
@@ -0,0 +1,18 @@
+//
+//  PalleteEditor.swift
+//  Emoji Art
+//
+//  Created by Радомир Радомир on 2/6/24.
+//
+
+import SwiftUI
+
+struct PalleteEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PalleteEditor()
+}
