d10d32f2cf99f17436fa22bef50768927d6c6f0f
Radomir Bryzgalov <rb8745359@gmail.com>
2024-02-09 14:22:07 -0500

    lec 13 finished

---
+++ Emoji Art/PaletteManager.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteManager.swift
+//  Emoji Art
+//
+//  Created by Радомир Радомир on 2/9/24.
+//
+
+import SwiftUI
+
+struct PaletteManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteManager()
+}
